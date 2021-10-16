f449896c26f7814ffa95f6ec476f2ef8bc42f65d
DReva <DReva@inwk.com>
2021-10-16 22:04:52 +0300

    Added CORS headers

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
 import cors from 'cors'
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE'
+}
 
 const login = 'direva99'
 
 export default function initApp(express, bodyParser, fs, crypto, http) {
-    const app = express();
+    const app = express()
     app
         .use(bodyParser.urlencoded({extended:true}))   
         .use(cors())    
         .all('/login/', r => {
-            r.res.send(login);
+            r.res.set(headers).send(login)
         })
         .all('/code/', r => {
+            r.res.set(headers)
             fs.readFile(import.meta.url.substring(7),(err, data) => {
-                if (err) throw err;
-                r.res.end(data);
-              });           
+                if (err) throw err
+                r.res.end(data)
+              })
         })
         .all('/sha1/:input/', r => {
-            r.res.send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
+            r.res.set(headers).send(crypto.createHash('sha1').update(r.params.input).digest('hex'))
         })
         .get('/req/', (req, res) =>{
-            let data = '';
+            res.set(headers)
+            let data = ''
             http.get(req.query.addr, async function(response) {
                 await response.on('data',function (chunk){
-                    data+=chunk;
+                    data+=chunk
                 }).on('end',()=>{})
                 res.send(data)
             })
         })
         .post('/req/', r =>{
-            const {addr} = req.body;
+            r.res.set(headers)
+            const {addr} = req.body
             r.res.send(addr)
         })
         .all('*', r => r.res.send(login))
         .use(({res:r})=>r.status(404).send(login))
-    return app;
+    return app
 }
